4c41a0030043d5dcc84b31f592b0272d608cbcdd
cKirkktoh <113542793+cKirkktoh@users.noreply.github.com>
2023-02-03 10:22:56 -0500

    added dnd
    added dnd

--- script/main.js
+++ script/main.js
@@ -7,6 +7,10 @@
 // the variable name is the "basket"
 let navButtons = document.querySelectorAll('#buttonHolder img'),
 	theHeadline = document.querySelector('#headLine h1');
+	// collect the draggable pieces
+	puzzlePieces = document.querySelectorAll('.puzzle-pieces img');
+	// collect ALL of th drop zone elements
+	dropZones = document.querySelectorAll('.drop-zone'),
 	puzzleBoard = document.querySelector('.puzzle-board')
 
 // functions go in the middle
@@ -20,8 +24,20 @@ function changeBGImage () {
 	puzzleBoard.style.backgroundImage = `url(images/backGround${this.id}.jpg)`;
 }
 
+function handleStartDrag() { console.log('started dragging a piece!', this); }
+
+function handleDragOver() { console.log('dragging over me!'); }
+
+function handleDrop() { console.log('dropped on me!'); }
+
 // event handling at the bottom -> how things react when you
 // how is the user going to interact with the elements / controls you provide?
 
 // process a collection of elements and add an event handler to each
 navButtons.forEach(button => button.addEventListener('click', changeBGImage));
+
+// add the drag start handler to all of the puzzle pieces
+puzzlePieces.forEach(piece => piece.addEventListener('dragstart', handleStartDrag));
+// add the dragover hnadling to the drop zones
+dropZones.forEach(zone => zone.addEventListener('dragover', handleDragOver));
+dropZones.forEach(zone => zone.addEventListener('drop', handleDrop));
